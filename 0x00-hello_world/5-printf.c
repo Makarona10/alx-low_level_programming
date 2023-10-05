0310a3629e399f01983fe445116d71ecc2278630
Makarona10 <ahmedneyssi@gmail.com>
2023-10-05 16:55:27 -0700

    s

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point for the program
+ *
+ * Description: program prints a sentence
+ *
+ * Return: Always 0 in case of success
+ **/
+
+int main (void) 
+{
+	printf("with proper grammar, but the outcome is a piece of art,");
+	return (0);
+}
