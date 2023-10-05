dc4728afc7a36715153cc228f0af74bd7ebd830e
Makarona10 <ahmedneyssi@gmail.com>
2023-10-05 15:25:08 -0700

    s

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,4 +1,11 @@
 #include<stdio.h>
+/**
+ * main - This is the entry point of the code.
+ * 
+ * Return: 0 refers to successful operation.
+ *
+ * Description: A C programm returns a sentence. 
+*/
 int main(){
 	puts("\"Programming is like building a multilingual puzzle \n");
 	return 0;
