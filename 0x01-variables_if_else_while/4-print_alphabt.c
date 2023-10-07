d2268a589a3065e1776d8fbb3ed51fca391a5c58
Makarona10 <ahmedneyssi@gmail.com>
2023-10-07 00:35:45 -0700

    s

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point for the prog
+ *
+ * Description: C Program prints the alphabet
+ *
+ * Return: always 0 when success
+*/
+int main(void)
+{
+	char letter;
+
+	for (letter = 'a'; letter <= 'z'; letter++)
+	{
+		if (letter == 'q' || letter == 'e')
+		{
+			continue;
+		}
+		putchar(letter);
+	}
+	putchar('\n');
+
+	return (0);
+}
