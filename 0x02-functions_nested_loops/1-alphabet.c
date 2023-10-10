4509f88aa9f65df4f564fee04a9102be94eb6a93
Makarona10 <ahmedneyssi@gmail.com>
2023-10-09 22:13:32 -0700

    s

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point for the prog
+ *
+ * Description: C Program prints alphabet
+ *
+ * Return: always 0 when success
+*/
+
+void print_alphabet(void);
+
+int main(void)
+{
+	print_alphabet();
+	return (0);
+}
+
+void print_alphabet(void)
+{
+	char letter;
+
+	for (letter = 'a'; letter <= 'z'; letter++)
+	{
+		putchar(letter);
+	}
+	putchar('\n');
+}
