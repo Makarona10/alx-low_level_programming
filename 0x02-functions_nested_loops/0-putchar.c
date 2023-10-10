3266330a1aaa71fcb61221f3cae62f3d5f8d9d14
Makarona10 <ahmedneyssi@gmail.com>
2023-10-10 11:00:21 -0700

    s

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -13,8 +13,9 @@ int main(void)
 	char word[] = "_putchar";
 	int x;
 
-	for (x = 0; x < 8; x++){
-		_putchar(word[x]);
+	for (x = 0; x < 8; x++)
+	{
+	_putchar(word[x]);
 	}
 	_putchar('\n');
 	return (0);
