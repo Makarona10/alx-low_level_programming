f638c31d232198cadb9e075becfeea0ba9584d0f
Makarona10 <ahmedneyssi@gmail.com>
2023-10-10 15:00:12 -0700

    s

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,15 @@
+include "main.h"
+
+/**
+ * print_last_digit - Function returns the last digit of an input
+ *
+ * @x: The integer number will be used
+ *
+ * Return:  the last digit of the input
+ *
+*/
+
+int print_last_digit(int x)
+{
+	return (x % 10);
+}
