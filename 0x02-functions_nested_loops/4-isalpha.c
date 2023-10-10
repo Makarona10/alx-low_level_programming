2b304229f5056c39dfd3ca7ab3865397ce4f10d5
Makarona10 <ahmedneyssi@gmail.com>
2023-10-10 13:06:17 -0700

    s

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _isalpha - A function checks for alphabets
+ *
+ * @c: the input will be checked if it is alphabet or not
+ *
+ * Return: always 1 when the input is letter and 0 if else
+*/
+
+int _isalpha(int c)
+{
+	if (c < 123 && c > 64)
+
+		return (1);
+
+	else
+		return (0);
+}
