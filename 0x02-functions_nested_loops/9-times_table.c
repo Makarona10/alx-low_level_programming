e2a91cf9e34727b2eb618ef759d319cbbc757aae
Makarona10 <ahmedneyssi@gmail.com>
2023-10-10 18:39:27 -0700

    s

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -17,18 +17,16 @@ void times_table(void)
 			num = x * z;
 			if (num > 9)
 			{
-				putchar((num / 10) + '0');
-				putchar((num % 10) + '0');
+				_putchar("  ");
+				_putchar((num / 10) + '0');
+				_putchar((num % 10) + '0');
 			}
 			if (num <= 9)
 			{
-				putchar(num + '0');
+				_putchar(" ");
+				_putchar(num + '0');
 			}
-			if (z < 9)
-			{
-				putchar(',');
-				putchar(' ');
-			}
+			_putchar(',');
 		}
 		putchar('\n');
 	}
