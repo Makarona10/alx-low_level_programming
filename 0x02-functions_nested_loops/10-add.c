b653518d58a80d4de7157335d9e155afc8d72cea
Makarona10 <ahmedneyssi@gmail.com>
2023-10-10 18:57:57 -0700

    s

--- 0x02-functions_nested_loops/10-add.c
+++ 0x02-functions_nested_loops/10-add.c
@@ -3,10 +3,14 @@
 /**
  * add - Function return the sum of 2 integer numbers
  *
- * Return:  the sum of both inputs
+ * Return: the sum of both inputs
+ *
+ * @x: The first input in sum operation
+ *
+ * @y: The second input in sum operation
 */
 
 int add(int x, int y)
 {
-	return(x + y);
+	return (x + y);
 }
