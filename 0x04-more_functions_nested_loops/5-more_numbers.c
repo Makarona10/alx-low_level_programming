e79303d14441442bcda270f24c0b18b41f7c6ba6
Makarona10 <ahmedneyssi@gmail.com>
2023-10-12 12:51:34 -0700

    s

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ * more_numbers - Print numbers from 0 to 14 for 10 times;
+ *
+ * Return: void
+*/
+
+void more_numbers(void)
+{
+	int x, y, z;
+
+	for (x = 0; x <= 9; x++)
+	{
+		for (y = 0; y <= 14; y++)
+		{
+			z = y;
+			if (y > 9)
+			{
+				z = y / 10;
+			}
+			putchar(z + '0');
+			if (y > 9)
+			{
+				_putchar((y % 10) + '0');
+			}
+		}
+		_putchar('\n');
+	}
+}
