53ffda23d37c072cc70711a9b7975b5ac52fec3d
Makarona10 <ahmedneyssi@gmail.com>
2023-10-12 17:06:12 -0700

    s

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -9,7 +9,7 @@
  * Return: void
 */
 
-void Fizz_Buzz_test(void)
+int main(void)
 {
 	int x;
 
@@ -17,17 +17,23 @@ void Fizz_Buzz_test(void)
 	{
 		if ((x % 3 == 0) && (x % 5 == 0))
 
-			printf("FizzBuzz ");
+			printf("FizzBuzz");
 
 		else if ((x % 3 == 0) && (x % 5 != 0))
 
-			printf("Fizz ");
+			printf("Fizz");
 
 		else if ((x % 5 == 0) && (x % 3 != 0))
 
-			printf("Buzz ");
+			printf("Buzz");
 
 		else
-			printf("%d ", x);
+			printf("%d", x);
+
+		if (x < 100)
+			printf(" ");
+		else 
+			printf("\n");
 	}
+	return (0);
 }
