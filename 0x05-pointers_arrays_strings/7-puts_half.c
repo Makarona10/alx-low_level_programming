5ad988e387d36eddf361323bb1c9f141689dc71d
Makarona10 <ahmedneyssi@gmail.com>
2023-10-15 23:55:26 -0700

    s

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ * puts_half - prints the second half of string
+ *
+ * @str: string argument will be used in print operation
+ *
+ * Return: void
+*/
+
+
+void puts_half(char *str)
+{
+	int x = 0;
+	int length = 0;
+
+	while (*(str + length) != 0)
+		length++;
+
+	if (length % 2 == 0)
+	{
+		for (x = (length / 2); x < length; x++)
+		_putchar(*(str + x));
+	}
+	else
+	{
+		for (x = ((length + 1) / 2); x < length; x++)
+		_putchar(*(str + x));
+	}
+}
