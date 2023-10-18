fa31dbc9c566104483f2192f22971865d8126d15
Makarona10 <ahmedneyssi@gmail.com>
2023-10-18 11:41:59 -0700

    s

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * _strcmp -  compares two strings
+ *
+ * @s1: The first string will be compared
+ *
+ * @s2: The second string will be compared
+ *
+ * Return: The difference between first two different chars
+*/
+
+int _strcmp(char *s1, char *s2)
+{
+	int eql = 0;
+
+	while (*s1)
+	{
+		if (*s1 != *s2)
+		{
+			eql = (*s1 - 48) - (*s2 - 48);
+			break;
+		}
+		*s1++;
+		*s2++;
+	}
+
+	return (eql);
+}
