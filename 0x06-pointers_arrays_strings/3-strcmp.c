36aeef2de942880a35647944f5f85e96f9ec6813
Makarona10 <ahmedneyssi@gmail.com>
2023-10-18 23:56:44 -0700

    s

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -15,11 +15,13 @@ int _strcmp(char *s1, char *s2)
 	int cmp = 0;
 	int x = 0;
 
-	while (s1[x] = '\0')
+	while (s1[x] != '\0')
 	{
 		if (s1[x] != s2[x])
-			cmp = s1[x] - s1[2];
-
+		{
+			cmp = s1[x] - s2[x];
+			break;
+		}
 		x++;
 	}
 	return (cmp);
