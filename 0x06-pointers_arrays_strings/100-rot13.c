6881399b11977bc21cedf80ca5913d2c8fa8b77b
Makarona10 <ahmedneyssi@gmail.com>
2023-10-19 01:54:53 -0700

    s

---
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * rot13 -  encodes a string using rot13
+ *
+ * @s: The string will be encoded
+ *
+ * Return: The String's been encoded
+*/
+
+char *rot13(char *s)
+{
+	int x = 0;
+
+	while (s[x] != '\0')
+	{
+		while ((s[x] >= 'a' && s[x] <= 'z') || (s[x] >= 'A' && s[x] <= 'Z'))
+		{
+			if ((s[x] >= 'a' && s[x] <= 'm') || (s[x] >= 'A' && s[x] <= 'M'))
+				s[x] += 13;
+
+			else
+				s[x] -= 13;
+			x++;
+		}
+		x++;
+	}
+	return (s);
+}
