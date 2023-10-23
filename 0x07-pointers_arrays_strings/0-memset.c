562c427bcfda3448f98122b81bf869686d6cdfea
Makarona10 <ahmedneyssi@gmail.com>
2023-10-23 05:13:33 -0700

    s

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * _memset - fills memory with a constant byte
+ *
+ * @s: The string will be inputted
+ *
+ * @b: the character will the place n times
+ *
+ * @n: Number of bytes will b take place
+ *
+ * Return: The String after replacing bytes
+*/
+
+char *_memset(char *s, char b, unsigned int n)
+{
+	int x;
+
+	for (x = 0; x < n; x++)
+		*(s + x) = b;
+
+	return (s);
+}
---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * _memcpy - copies memory area
+ *
+ * @src: The memory area will be copied
+ *
+ * @dest: The destination memory area
+ *
+ * @n: Number characters will be copied to destination
+ *
+ * Return: The destination after updating
+*/
+
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	int x;
+
+	for (x = 0; x < n; x++)
+	{
+		dest[x] = src[x];
+	}
+	return (dest);
+}
