58594330544ca70cf6d8a4e23efebfa0a9a7eea5
Makarona10 <ahmedneyssi@gmail.com>
2023-10-25 00:17:04 -0700

    s

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -10,11 +10,11 @@
 
 void _puts_recursion(char *s)
 {
-	if (*s != '\0')
+	if (*s == '\0')
+		_putchar('\0');
+	else
 	{
 		_putchar(*s);
-		_puts_recursion(s + 1);
+		_puts_recursion(++s);
 	}
-	else
-		_putchar('\0');
 }
