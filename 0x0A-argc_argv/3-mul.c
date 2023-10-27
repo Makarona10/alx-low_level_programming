868b2f3636bf82502e72c8efca8ad86be906fc6e
Makarona10 <ahmedneyssi@gmail.com>
2023-10-27 04:02:06 -0700

    s

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -17,11 +17,14 @@ int main(int argc, char **argv)
 	int result;
 
 	if (argc != 3)
+	{
 		printf("ERROR\n");
+		return (0);
+	}
 	else
 	{
 		result = atoi(argv[1]) * atoi(argv[2]);
 		printf("%d\n", result);
 	}
-	return (1);
+	return (0);
 }
