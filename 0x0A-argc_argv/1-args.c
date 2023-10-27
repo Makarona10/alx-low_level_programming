3172394b1778b949802f783893175802d8462e71
Makarona10 <ahmedneyssi@gmail.com>
2023-10-27 03:49:37 -0700

    done

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -3,20 +3,21 @@
 #include <stdio.h>
 
 /**
- * main - The entry point of the program
+ * main - program prints the number of arguments
+ * passed into it
  *
  * @argc: stores the number of command-line arguments passed to the program
  *
  * @argv: array of character pointers that
  * point to the command-line arguments
  *
- * Return: always 1
+ * Return: always 0
 */
 
 
-int main(int argc, char **argv)
+int main(int argc, char **argv __attribute__((unused)))
 {
-	printf("%d\n", argc);
+	printf("%d\n", (argc - 1));
 
-	return (1);
+	return (0);
 }
