df442d0cdd33d362965dca2db936bba4958e72a5
Makarona10 <ahmedneyssi@gmail.com>
2023-10-30 01:59:14 -0700

    s

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -2,53 +2,6 @@
 #include "main.h"
 
 /**
- * _to_hexa - Transform the input to hexa deximal
- *
- * @dec: The number will be transformed
- *
- * Return: void
-*/
-
-void _to_hexa(int dec)
-{
-	char hex[10];
-	int remain;
-	int x = 0;
-
-	if (dec == 0)
-	{
-		putchar('0');
-		putchar('0');
-	}
-	else if (dec < 9)
-	{
-		_putchar('0');
-		_putchar('0' + dec);
-	}
-	else
-	{
-		while (dec > 9)
-		{
-			remain = dec % 16;
-
-			if (remain > 9)
-				hex[x] = 'A' + (remain - 10);
-
-			else
-				hex[x] = ('0' + remain);
-
-			x++;
-			dec = (dec / 16);
-		}
-	}
-	while (x--)
-	{
-		_putchar(hex[x]);
-	}
-		_putchar(' ');
-}
-
-/**
  * create_array - creates an array of chars,
  * and initializes it with a specific char
  *
@@ -62,21 +15,15 @@ void _to_hexa(int dec)
 char *create_array(unsigned int size, char c)
 {
 	char *ch = (char *)malloc(size);
-	char cc = c;
-	unsigned int x;
-
-	ch = &cc;
+	int x;
 
 	if (size == 0 || ch == 0)
 		return (NULL);
 
 	for (x = 0; x < size; x++)
 	{
-		_putchar('0');
-		_putchar('x');
-		_to_hexa(c);
+		*(n + x) = c;
 	}
-
 
 	return (ch);
 }
