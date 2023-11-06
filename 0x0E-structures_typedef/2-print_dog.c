c590e384bae33033df2756cdfd9566863024d2e2
Makarona10 <ahmedneyssi@gmail.com>
2023-11-06 00:16:14 -0800

    s

---
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -0,0 +1,21 @@
+#include "dog.h"
+#include <stdlib.h>
+
+/**
+ * print_dog - creates an array of integers
+ *
+ * @d: a struct its properties will be printed
+ *
+ * Return: void
+*/
+
+void print_dog(struct dog *d)
+{
+
+	if (d)
+	{
+		printf("Name: %s\n", d->name ? d->name : "nil");
+		printf("Age: %f\n", d->age);
+		printf("Owner: %s\n", d->owner ? d->owner : "nil");
+	}
+}
---
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -0,0 +1,62 @@
+#include "dog.h"
+#include <stdlib.h>
+
+/**
+* get_len - gets the length of a string
+*
+* @s: The string input to calculate the length of
+*
+* Return: The length of a string
+*/
+
+int get_len(char *s)
+{
+	int len = 0;
+
+	if (s != NULL)
+	{
+		while (*s++ != '\0')
+			len++;
+
+		s -= (len + 1);
+	}
+	return (len);
+}
+
+/**
+ * new_dog - creates a new dog of struct dog
+ *
+ * @name: The name of a new dog
+ *
+ * @age: The age of a new dog
+ *
+ * @owner: The new dog's owner's name
+ *
+ * Return: void
+*/
+
+dog_t *new_dog(char *name, float age, char *owner)
+{
+	dog_t *ndog = malloc(sizeof(dog_t));
+
+	if (ndog == NULL)
+		return (NULL);
+
+	ndog->name = malloc(get_len(name) + 1);
+	if (ndog->name == NULL)
+	{
+		free(ndog);
+		return (NULL);
+	}
+	ndog->owner = owner;
+	if (ndog->owner == NULL)
+	{
+		free(ndog);
+		return (NULL);
+	}
+	ndog->name = name;
+	ndog->age = age;
+	ndog->owner = owner;
+
+	return (ndog);
+}
