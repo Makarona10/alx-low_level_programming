d483da94f24994fd7112efd2c5d5dd2be225bb4e
Makarona10 <ahmedneyssi@gmail.com>
2023-11-06 07:40:35 -0800

    s

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -75,13 +75,13 @@ dog_t *new_dog(char *name, float age, char *owner)
 	ndog->name = malloc(get_len(name) + 1);
 	if (ndog->name == NULL)
 	{
-		free(ndog->name);
 		free(ndog);
 		return (NULL);
 	}
 	ndog->owner = malloc(get_len(owner) + 1);
 	if (ndog->owner == NULL)
 	{
+		free(ndog->name);
 		free(ndog);
 		return (NULL);
 	}
