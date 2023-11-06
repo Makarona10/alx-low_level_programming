ba7b5e84a542fce427ba666488f6fd9b4eefc814
Makarona10 <ahmedneyssi@gmail.com>
2023-11-06 05:51:17 -0800

    s

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -37,7 +37,7 @@ int get_len(char *s)
 
 dog_t *new_dog(char *name, float age, char *owner)
 {
-	if (name == NULL || age < 0 || owner == NULL)
+	if (!name || age < 0 || !owner)
 		return (NULL);
 
 	dog_t *ndog = malloc(sizeof(dog_t));
