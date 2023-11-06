b5aa40fcd422845b095696489e1787b1efc3b69b
Makarona10 <ahmedneyssi@gmail.com>
2023-11-06 07:52:43 -0800

    s

--- 0x0E-structures_typedef/5-free_dog.c
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -1,7 +1,16 @@
 #include "dog.h"
 #include <stdlib.h>
 
+/**
+ * free_dog - frees memory
+ *
+ * @d: The allocated memory that will be freed
+ *
+ * Return: void
+*/
+
 void free_dog(dog_t *d)
 {
-	free(d);
+	if (d)
+		free(d);
 }
