2003463a7082b4ed6e58196c78d63b3c22ca7543
Makarona10 <ahmedneyssi@gmail.com>
2023-11-16 10:20:30 -0800

    s

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -1,6 +1,27 @@
 #include "lists.h"
 
 /**
+* get_len - gets the length of a string
+*
+* @s: The string input to calculate the length of
+*
+* Return: The length of a string
+*/
+
+int get_len(char *s)
+{
+	int len = 0;
+	int x = 0;
+
+	if (s != NULL)
+	{
+		while (s[x++] != '\0')
+			len++;
+	}
+	return (len);
+}
+
+/**
  * print_list - prints all the elements of a list_t list
  *
  * @h: The header of a list
--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -28,19 +28,7 @@ typedef struct list_s
 * Return: The length of a string
 */
 
-int get_len(char *s)
-{
-        int len = 0;
-        int x = 0;
-
-        if (s != NULL)
-        {
-                while (s[x++] != '\0')
-                        len++;
-        }
-        return (len);
-}
-
+int get_len(char *s);
 int _putchar(char c);
 size_t print_list(const list_t *h);
 size_t list_len(const list_t *h);
