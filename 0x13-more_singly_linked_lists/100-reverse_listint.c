5d06ae8507a1abd5e671e98cfb1d60a6cc80e50d
Makarona10 <ahmedneyssi@gmail.com>
2023-11-21 04:36:36 -0800

    s

--- 0x13-more_singly_linked_lists/100-reverse_listint.c
+++ 0x13-more_singly_linked_lists/100-reverse_listint.c
@@ -15,6 +15,9 @@ listint_t *reverse_listint(listint_t **head)
 	if (!head || !(*head))
 		return (NULL);
 
+	if (!(*head)->next)
+		return (*head);
+
 	if (!(*head)->next->next)
 	{
 		(*head)->next->next = (*head);
