543e77b0ef10781ed56f072d4b799683086b2d0d
Makarona10 <ahmedneyssi@gmail.com>
2023-11-21 01:04:35 -0800

    s

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -30,6 +30,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 		{
 			node->next = (*head);
 			node->n = n;
+			*head = node;
 			return (node);
 		}
 		if (x == (idx - 1))
