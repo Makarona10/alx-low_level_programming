8a4296b9ce692a6aeb9d37c01fd0b5fa1a2233a8
Makarona10 <ahmedneyssi@gmail.com>
2023-11-21 00:56:08 -0800

    s

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -24,9 +24,9 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
 	{
 		if (index == 0)
 		{
-			*head = (*head)->next;
-			current->next = NULL;
-			free(current);
+			current = current->next;
+			free(*head);
+			*head = current;
 			return (1);
 		}
 		if (x == (index - 1))
