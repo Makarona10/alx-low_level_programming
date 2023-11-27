6239863e6c5ec5d33caba1e83aee921f29c5744b
Makarona10 <ahmedneyssi@gmail.com>
2023-11-27 11:46:44 -0800

    s

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -39,7 +39,7 @@ int create_file(const char *filename, char *text_content)
 	if (!filename)
 		return (-1);
 
-	f = open(filename, O_WRONGLY | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR);
+	f = open(filename, O_WRONLY | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR);
 
 	if (f == -1)
 		return (-1);
