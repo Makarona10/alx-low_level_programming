48b38629e06650c1edaed3b4b54bef380546b8c8
Makarona10 <ahmedneyssi@gmail.com>
2024-03-21 04:22:22 +0200

    s

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -4,17 +4,17 @@
 
 def island_perimeter(grid):
     """ Island Perimeter func """
-    per = 0
-    x = []
+    peri = 0
+    gl = []
     for i in range(len(grid)):
         for j in range(len(grid[i])):
-            x.append(grid[i][j])
-    for i in range(len(x)):
-        if i < len(x) - 1 and i > 0:
-            if x[i] == 0 and x[i + 1] == 1 or x[i] == 0 and x[i - 1] == 1:
-                per += 1
-    if per % 2 == 0:
-        per *= 2
+            gl.append(grid[i][j])
+    for i in range(len(gl)):
+        if i < len(gl) - 1 and i > 0:
+            if gl[i] == 0 and gl[i + 1] == 1 or gl[i] == 0 and gl[i - 1] == 1:
+                peri += 1
+    if peri % 2 == 0:
+        peri *= 2
     else:
-        per = (per * 2) - 1
-    return per
+        peri = (peri * 2) - 1
+    return peri
