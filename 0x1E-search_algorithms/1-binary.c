ae4c6bd4ac6361cfe5e7f12395aa9eadfb0bfd05
Makarona10 <ahmedneyssi@gmail.com>
2024-05-05 04:43:09 +0300

    s

---
+++ 0x1E-search_algorithms/1-binary.c
@@ -0,0 +1,45 @@
+#include "search_algos.h"
+
+/**
+ * binary_search - searches for a value in a sorted array of
+ * integers using the Binary search algorithm
+ *
+ * @array: An array will be searched
+ *
+ * @size: Size of the array
+ *
+ * @value: The value will be searched for
+ *
+ * Return: The first index where value is located
+ */
+
+int binary_search(int *array, size_t size, int value)
+{
+	size_t mid, x;
+	int l, h;
+
+	h = size - 1;
+	l = 0;
+	mid = size;
+
+	for (; size > 0; size--)
+	{
+		printf("Searching in array: ");
+		for (x = l; x <= h; x++)
+		{
+			if (x < h)
+				printf("%d, ", array[x]);
+			else
+				printf("%d\n", array[x]);
+		}
+		mid = (h + l) / 2;
+		if (array[mid] == value)
+			return (mid);
+		if (value < array[mid])
+			h = mid - 1;
+		else
+			l = mid + 1;
+	}
+
+	return (-1);
+}
--- 0x1E-search_algorithms/main.c
+++ 0x1E-search_algorithms/main.c
@@ -2,21 +2,30 @@
 
 int linear_search(int *array, size_t size, int value)
 {
-    size_t mid;
+    size_t mid, x;
     int l, h;
 
     h = size - 1;
     l = 0;
+    mid = size;
 
     for (; size > 0; size--)
     {
+        printf("Searching in array: ");
+        for (x = l; x <= h; x++)
+        {
+            if (x < h)
+                printf("%d, ", array[x]);
+            else
+                printf("%d\n", array[x]);
+        }
         mid = (h + l) / 2;
         if (array[mid] == value)
             return (mid);
         if (value < array[mid])
             h = mid - 1;
         else
-            l = mid + 1; 
+            l = mid + 1;
     }
 
     return (-1);
@@ -41,9 +50,10 @@ int linear_search(int *array, size_t size, int value)
     //     linear_search((current), (size / 2), value);
 }
 
-int main (void) {
+int main(void)
+{
     int arr[] = {1, 2, 3, 4, 5, 7, 8, 9, 13, 15};
-    int x = linear_search(arr, 10, 1);
+    int x = linear_search(arr, 10, 3);
     printf("%d\n", x);
     return 1;
 }
